7c941f70fa90d2721e1f3de27f5aa9888d6f5832
stephen <nwabuezesc@gmail.com>
2022-04-29 17:42:57 +0100

    comment commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,20 @@
-
+//importing react 
 import React from 'react'
+//importing card and other component from material UI
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+//import countup to start a count up animation
 import CountUp from 'react-countup';
+
+//import classname to allow load in multiple styles in a component
 import cx from 'classnames'
+
+//importing styles for the cards component
 import styles from './Cards.module.css'
 
+
+//functional component passing the data
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate}}) => {
+  //conditional statement for the confirmation
   if (!confirmed){
     return 'loading...'
   }
@@ -15,7 +24,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate}}) => {
     <div className={styles.container}>
       <Grid container spacing={3} justifyContent="center">
         {/* type of grid is item */}
-        {/* create a card component inside the grid followed by the content */}
+        {/* create a card component inside the grid followed by the content and passing the data value */}
         <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>Infected</Typography>
@@ -54,4 +63,5 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate}}) => {
   )
 }
 
+//exporting cards
 export default Cards
